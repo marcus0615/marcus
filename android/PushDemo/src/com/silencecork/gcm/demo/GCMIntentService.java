d56af998c2c33a59cfc4ff61ec2bfd7b10d9d5f5
justin <justin@gomaji.com>
2015-10-20 18:26:28 +0800

    add push dialog when receiving GCM

--- android/PushDemo/src/com/silencecork/gcm/demo/GCMIntentService.java
+++ android/PushDemo/src/com/silencecork/gcm/demo/GCMIntentService.java
@@ -1,22 +1,38 @@
 package com.silencecork.gcm.demo;
 
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
 import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
+
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GCMIntentService extends IntentService {
 
 	NotificationManager mNotificationManager;
 	NotificationCompat.Builder builder;
+	private PowerManager mPowerManager;
 
 	public GCMIntentService() {
 		super("GCMIntentService");
 	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+	}
+
+
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
@@ -31,9 +47,8 @@ public class GCMIntentService extends IntentService {
 
 			// read extras as sent from server
 			String message = extras.getString("message");
-			String serverTime = extras.getString("timestamp");
-			sendNotification("Message: " + message + "\n" + "Server Time: "
-					+ serverTime);
+			sendNotification("Message: " + message);
+			startPushActivity(message);
 		}
 		// Release the wake lock provided by the WakefulBroadcastReceiver.
 		GCMBroadcastReceiver.completeWakefulIntent(intent);
@@ -53,6 +68,34 @@ public class GCMIntentService extends IntentService {
 
 		mBuilder.setContentIntent(contentIntent);
 		mNotificationManager.notify((int)System.currentTimeMillis(), mBuilder.build());
+	}
+	
+	@SuppressWarnings("deprecation")
+	private void startPushActivity(String message) {
+		// if the to activity is PushNotification, do not start new activity
+		// or screen is opened.
+		if (getTopActivityName().equals(PushNotificationActivity.class.getName()) ||
+				!mPowerManager.isScreenOn()) {
+			Intent intent = new Intent(this, PushNotificationActivity.class);
+			intent.putExtra("message", message);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public String getTopActivityName() {
+		String activityName = "";
+		try {
+			ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+			List<RunningTaskInfo> forGroundActivity = activityManager
+					.getRunningTasks(1);
+			RunningTaskInfo currentActivity;
+			currentActivity = forGroundActivity.get(0);
+			activityName = currentActivity.topActivity.getClassName();
+		} catch (Exception e) {
+		}
+		return activityName;
 	}
 
 }
